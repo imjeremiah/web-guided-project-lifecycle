851663b99c6d2e56bfa2a09e804606105ad46146
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-11 16:48:48 -0500

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import axios from 'axios';
 
+
+class ImageList extends React.Component {
+    render() {
+        return (<div>
+            {
+                this.props.dogImages.map(image=> {
+                    return <img key={image} width="300" src={image}/>
+                })
+            }
+        </div>);
+    }
+}
+
+
+
 class App extends React.Component {
     state = {
         dogImages: [],
