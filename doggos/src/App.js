1792d95ccb3218da9babee80414cca4459cb54dc
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-11 13:46:43 -0500

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -3,16 +3,14 @@ import axios from 'axios';
 
 class App extends React.Component {
     state = {
-        dogImages: [
-            "https://images.dog.ceo/breeds/husky/20180901_150234.jpg",
-            "https://images.dog.ceo/breeds/husky/20180904_185604.jpg",
-            "https://images.dog.ceo/breeds/husky/20180924_193829.jpg"
-        ]
+        dogImages: []
+    }
+
+    componentDidMount() {
+
     }
 
     render() {
-        console.log(this.state);
-
         return (<div>
             <h1>Dog Image Query 3000</h1>
             <form>
